20c7f7d23330f42953577fc09943ca78b5b1800d
Tim van Andel <532882@student.glu.nl>
2021-02-01 22:53:00 +0100

    portfolio

---
+++ routes/web.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/overmij', function () {
+    return view('overmij');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/mijnwerk', function () {
+    $posts = App\Post::all();
+    return view('mijnwerk', compact('posts'));
+});
+
+Route::get('post/{slug}', function($slug){
+	$post = App\Post::where('slug', '=', $slug)->firstOrFail();
+	return view('post', compact('post'));
+});
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
